97ee8a95914579c29453c5974aa28e1be74d9916
Narendra Kumar <kumarnarendra75524@gmail.com>
2023-09-14 20:45:44 +0000

    file is added to repo

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '@5&-q%^o=@mb@=@e%b9yz^b#l-2)w&_s0ick#=wy3kw36$z($g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
